cc4c71521b38e9471b0fe9be636571428ada9326
CarolHaberland <carolhaberland@gmail.com>
2022-10-18 19:18:49 -0300

    Variaveis

---
+++ exercicio1/script.js
@@ -0,0 +1,29 @@
+
+
+/* let nome = 'Carol'
+const idade = 24
+
+console.log(typeof nome, typeof idade) */
+
+/* let nome = prompt('Qual seu nome?')
+const idade = prompt('Qual sua idade?')
+
+console.log(nome)
+console.log(idade)
+
+console.log(typeof nome)
+console.log(typeof idade)
+console.log('Olá,', nome, ',você tem', idade, 'anos.') */
+
+let pergunta1 = 'Você gosta de café?';
+let resposta1 = prompt(pergunta1)
+
+let pergunta2 = 'Você entende que café é melhor que chá?';
+let resposta2 = prompt(pergunta2)
+
+let pergunta3 = 'Você já bebeu água hoje?';
+let resposta3 = prompt(pergunta3)
+
+console.log(pergunta1, resposta1)
+console.log(pergunta2, resposta2)
+console.log(pergunta3, resposta3) 
---
+++ exercicio3/script.js
@@ -0,0 +1,54 @@
+
+
+let nome = 'Escreva seu nome completo:';
+let respostaNome = prompt(nome)
+
+const dataNasc = 'Informe sua data de nascimento:'
+const respostaData = Number(prompt(dataNasc))
+
+let endereco = 'Informe seu endereço:'
+let respostaEnd = prompt(endereco)
+
+const cpf = 'Informe seu CPF:'
+const respostaCpf = Number(prompt(cpf))
+
+let cnh = 'Possui CNH?'
+let respostaCNH = prompt(cnh)
+
+let qtdeFilhos = 'Possui quantos filhos?'
+let respostaFilhos = Number(prompt(qtdeFilhos))
+
+let cargo = 'Qual seu cargo atual?'
+let respostaCargo = prompt(cargo)
+
+let salario = 'Qual seu salario atual?'
+let respostaSalario = Number(prompt(salario))
+
+let comissao = 'Recebe comissão? Se sim, informe a porcentagem, caso não, responda com zero'
+let respostaComissao = Number(prompt(comissao))
+
+let dataAdmissao = 'Informe o ano de admissão:'
+let respostaAdmissao = Number(prompt(dataAdmissao))
+
+console.log(nome, respostaNome)
+console.log(dataNasc, respostaData)
+console.log(endereco, respostaEnd)
+console.log(cpf, respostaCpf)
+console.log(cnh, respostaCNH)
+console.log(qtdeFilhos, respostaFilhos)
+console.log(cargo, respostaCargo)
+console.log(salario, respostaSalario)
+console.log(comissao, respostaComissao)
+console.log(dataAdmissao, respostaAdmissao)
+
+console.log(typeof nome, typeof respostaNome)
+console.log(typeof dataNasc, typeof respostaData)
+console.log(typeof endereco, typeof respostaEnd)
+console.log(typeof cpf, typeof respostaCpf)
+console.log(typeof cnh, typeof respostaCNH)
+console.log(typeof qtdeFilhos, typeof respostaFilhos)
+console.log(typeof cargo, typeof respostaCargo)
+console.log(typeof salario, typeof respostaSalario)
+console.log(typeof comissao, typeof respostaComissao)
+console.log(typeof dataAdmissao, typeof respostaAdmissao)
+
